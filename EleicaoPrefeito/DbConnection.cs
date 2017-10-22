813ca666217bfc1156f8a8d635fb7bc803cb3c39
danPeres97 <danilo.peres@hotmail.com>
2017-10-22 09:54:52 -0200

    Adding Db Connection

---
+++ EleicaoPrefeito/DbConnection.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EleicaoPrefeito
+{
+    public class DbConnection
+    {
+        private string connetionString;
+        private SqlConnection sqlConnection;
+
+        public string ConnetionString { get; private set; }
+
+        public DbConnection(string connetionString)
+        {
+            this.ConnetionString = connetionString;
+            this.sqlConnection = new SqlConnection(connetionString);
+        }
+
+        public void OpenConnection(){
+          try{
+            this.sqlConnection.Open();
+          }
+          catch(Exception ex){
+            MessageBox.Show("Cannot open a connection!");
+          }
+        }
+      }
+}
