bd5dde7d956332713098f95f11d45886a8e1f959
danPeres97 <danilo.peres@hotmail.com>
2017-10-22 09:12:53 -0200

    Refactor Code adding classes Candidate and Party with Tests

---
+++ EleicaoPrefeito/Party.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EleicaoPrefeito
+{
+    public class Party
+    {
+        private string name;
+        private string initials;
+
+        public Party(string name, string initials)
+        {
+            this.Name = name;
+            this.Initials = initials;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public string Initials { get => initials; set => initials = value; }
+    }
+}
